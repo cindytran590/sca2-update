5adcdaa7bad212ea8e2c4dabe5505e6c8039a46f
Cindy <ctran33@bostonk12.org>
2019-07-17 16:45:54 -0400

    3_PM solutions

--- 3_PM/src/App.js
+++ 3_PM/src/App.js
@@ -14,18 +14,19 @@ class App extends Component {
   }
 
   updateGuess = event => {
-    this.setState({ guess: event.target.value })
+    this.setState({ guess: parseInt(event.target.value) })
   }
-
   checkWin = event => {
     event.preventDefault()
     // TODO: check if guess equal to randomValue and update this.state.win
+    if (this.state.guess === this.state.randomValue) {
+      this.setState({ win: true })
+    }
   }
 
   render() {
     return (
       <div className="App">
-        <div>Current value: {this.state.randomValue}</div>
         <form>
           <label>
             Enter a guess:
@@ -38,6 +39,16 @@ class App extends Component {
           <input type="submit" value="Submit" onClick={this.checkWin} />
         </form>
         {this.state.win && <WinMessage />}
+
+        <button
+          onClick={() =>
+            this.setState({
+              randomValue: Math.floor(Math.random() * 100)
+            })
+          }
+        >
+          Generate a New Number{" "}
+        </button>
       </div>
     )
   }

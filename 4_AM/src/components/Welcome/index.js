0d51b68472b4696236c4ea03c5cfb5daf540e5af
Spencer Tiberi <spencertiberi@gmail.com>
2019-07-17 01:06:05 -0400

    Init

---
+++ 4_AM/src/components/Welcome/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Container, Text } from "./styles"
+
+const Welcome = () => (
+  <Container>
+    <Text>SCA 2 Web Starter!</Text>
+  </Container>
+)
+
+export default Welcome
